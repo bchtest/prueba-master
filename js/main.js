c6b8af2adfb293dabce07cc64ddb4557ba138328
Sebastian Fuenzalida <s.fuenzalida5587@gmail.com>
2018-09-26 16:11:20 -0300

    Agrega AngularJS y Bootstrap UI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+angular.module('busquedaTabla', ['ui.bootstrap'])
+
+.controller('busquedaController', ['$scope', function($scope){
+    $scope.lista = [
+        {lorem: 'aaaaaa', ipsum: 'aaaaaa', dolor:'aaaaaa', sit:'aaaaaa', amet:'$ 200.000'},
+        {lorem: 'bbbbbb', ipsum: 'bbbbbb', dolor:'bbbbbb', sit:'bbbbbb', amet:'$ 100.000'},
+        {lorem: 'cccccc', ipsum: 'cccccc', dolor:'cccccc', sit:'cccccc', amet:'$ 50.000'},
+        {lorem: 'dddddd', ipsum: 'dddddd', dolor:'dddddd', sit:'dddddd', amet:'$ 200.000'},
+        {lorem: 'eeeeee', ipsum: 'eeeeee', dolor:'eeeeee', sit:'eeeeee', amet:'$ 100.000'},
+        {lorem: 'ffffff', ipsum: 'ffffff', dolor:'ffffff', sit:'ffffff', amet:'$ 50.000'},
+        {lorem: 'gggggg', ipsum: 'gggggg', dolor:'gggggg', sit:'gggggg', amet:'$ 200.000'},
+        {lorem: 'hhhhhh', ipsum: 'hhhhhh', dolor:'hhhhhh', sit:'hhhhhh', amet:'$ 100.000'},
+        {lorem: 'iiiiii', ipsum: 'iiiiii', dolor:'iiiiii', sit:'iiiiii', amet:'$ 50.000'}
+    ];
+}]);
